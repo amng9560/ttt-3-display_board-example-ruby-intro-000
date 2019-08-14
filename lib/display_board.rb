4877e1bb964bc7d8531c6e36e0dd40594405a2f0
Amy Nguyen <amy.nguyen730@gmail.com>
2019-08-14 23:53:08 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,5 +1,9 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
 def display_board
+<<<<<<< HEAD
+=======
+  puts "A Tic Tac Toe Board"
+>>>>>>> 2f375c44c6fc52760f69b2f250f45e49255c1699
   puts "   |   |   "
   puts "-----------"
   puts "   |   |   "
